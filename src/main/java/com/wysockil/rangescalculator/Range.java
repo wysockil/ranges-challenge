f91e783d177143cc69cddb1a4702f3215705e9e6
lukasz.wysocki <lukwys@gmail.com>
2022-08-20 12:26:51 +0200

    interface

---
+++ src/main/java/com/wysockil/rangescalculator/Range.java
@@ -0,0 +1,21 @@
+package com.wysockil.rangescalculator;
+
+public class Range {
+    private final Integer begin;
+    private final Integer end;
+
+    public Range(Integer begin, Integer end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public Integer getBegin() {
+        return begin;
+    }
+
+    public Integer getEnd() {
+        return end;
+    }
+
+
+}
---
+++ src/main/java/com/wysockil/rangescalculator/RangesCalculator.java
@@ -0,0 +1,7 @@
+package com.wysockil.rangescalculator;
+
+import java.util.List;
+
+public interface RangesCalculator {
+    int count(List<Range> ranges);
+}
