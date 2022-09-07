7f12c64188a2cee63d30b7682dac9c71506dd3ea
lukasz.wysocki <lukwys@gmail.com>
2022-09-07 07:47:30 +0200

    tests

--- src/test/java/com/wysockil/rangescalculator/RangesSetTest.java
+++ src/test/java/com/wysockil/rangescalculator/RangesSetTest.java
@@ -17,8 +17,28 @@ class RangesSetTest {
     public void shouldCountNonTouchingRanges() {
         collectRanges(new Range(1, 2), new Range(3, 4));
 
-        assertEquals(rangeSet.countOfRanges(), 2);
+        assertEquals(2, rangeSet.countOfRanges());
+    }
+
+    @Test
+    public void shouldCountNonTouchingRangesWithCommonPart() {
+        collectRanges(new Range(1, 3), new Range(2, 4), new Range(5, 6), new Range(7, 8));
+
+        assertEquals(3, rangeSet.countOfRanges());
     }
 
 
+    @Test
+    public void shouldCountNonTouchingRangesWithWrapperRange() {
+        collectRanges(new Range(1, 3), new Range(2, 4), new Range(0, 6));
+
+        assertEquals(1, rangeSet.countOfRanges());
+    }
+
+    @Test
+    public void shouldReturnZeroWhenRangesCollectionIsEmpty() {
+        collectRanges();
+
+        assertEquals(0, rangeSet.countOfRanges());
+    }
 }
